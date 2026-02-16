1b33fb7f05b54a02864847e77317dfc77823979a
ChiomaE <chioma.eriken@gmail.com>
2026-02-16 11:28:14 -0600

    display word api data, update readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Eriken-Chioma_DictionarySite_Capstone
 
+# Overview
+
+This website uses a dictionary api (referenced in Tools Used section) to access word definitions based on user input.
+
+# Tools Used
+
+Languages - HMTL, CSS, Javascript
+API: Free Dictionary Api - (https://dictionaryapi.dev/)
+Frontend Mentor (reference) -
+
+# What I Learned
+
+I expanded my knowledge of vanilla Javascript. Now, I am more confident in my ability to build websites using this alone. I also apply my experience whenever I use Javascript based frameworks, such as React and Angular.
+
+# Future Improvements
+
+In the future, I would like to implement a framework (React, Angular) that would allow me to make the page more dynamic.
--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 let submitBtn = document.getElementById("submitBtn");
+let wordDefMainEl = document.getElementById("wordDefinition");
+let fetchedWordEl = document.getElementById("fetchedWord");
+let noWordEl;
 
 function getWordDefinition() {
   let wordInput = document.getElementById("wordInput");
-  console.log("word:", wordInput);
+  if (!wordDefMainEl.hidden) {
+    wordDefMainEl.hidden = true;
+    if (!fetchedWordEl.hidden) {
+      fetchedWordEl.hidden = true;
+    }
+  }
   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}`)
-    .then((response) => {
-      return response.json();
-    })
-    .then((word) => {
-      console.log(word);
+    .then((response) =>
+      response.json().then((data) => ({ ok: response.ok, data })),
+    )
+    .then(({ ok, data }) => {
+      if (!ok) {
+        noWordEl = document.createElement("h4");
+        noWordEl.textContent = data.message;
+        wordDefMainEl.appendChild(noWordEl);
+      } else {
+        if (noWordEl) {
+          wordDefMainEl.removeChild(noWordEl);
+        }
+        console.log(data.word);
+        let wordDefEL = document.createElement("p");
+        wordDefEL.innerText = data[0].word;
+        fetchedWordEl.appendChild(wordDefEL);
+        fetchedWordEl.hidden = false;
+      }
+
+      wordDefMainEl.hidden = false;
+      console.log(wordDefMainEl);
     });
 }
 
