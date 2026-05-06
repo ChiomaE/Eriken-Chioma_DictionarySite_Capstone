cb061e105ec7d4eeb3fdfe4be581e64cfd6bd1f2
ChiomaE <chioma.eriken@gmail.com>
2026-05-06 11:04:25 -0500

    update readme, fix bug

--- README.md
+++ README.md
@@ -2,18 +2,139 @@
 
 # Overview
 
-This website uses a dictionary api (referenced in Tools Used section) to access word definitions based on user input.
+The Free Dictionary API (referenced in Tools Used section) was created to provide the internet with a free dictionary resource. This website uses the dictionary api to access word definitions based on user input.
+
+Requirements to run the application:
+
+- A modern web browser (Chrome, Edge, Firefox)
+- Internet connection (required for API requests)
+
+Steps to run the application:
+
+1. Clone repository
+   git clone <your-repo-url>
+2. Navigate to the project folder
+   cd dictionary-app
+3. Open file 'index.html' in your browser
 
 # Tools Used
 
 Languages - HMTL, CSS, Javascript
 API: Free Dictionary Api - (https://dictionaryapi.dev/)
-Frontend Mentor (reference) - (https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL)
+Frontend Mentor (design reference) - (https://www.frontendmentor.io/challenges/dictionary-web-app-h5wwnyuKFL)
+Star Icons - https://www.flaticon.com/free-icons/star
+
+# API Information
+
+URL Syntax: https://api.dictionaryapi.dev/api/v2/entries/en/<word>
+
+The application stores the user's input in the string variable wordInput. The value of wordInput is sent into the API, as shown below:
+
+https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}
+
+If the word exists, the API will return a word object inside of an array, as shown below:
+
+[
+{
+"word": "hello",
+"phonetic": "həˈləʊ",
+"phonetics": [
+{
+"text": "həˈləʊ",
+"audio": "//ssl.gstatic.com/dictionary/static/sounds/20200429/hello--_gb_1.mp3"
+},
+{
+"text": "hɛˈləʊ"
+}
+],
+"origin": "early 19th century: variant of earlier hollo ; related to holla.",
+"meanings": [
+{
+"partOfSpeech": "exclamation",
+"definitions": [
+{
+"definition": "used as a greeting or to begin a phone conversation.",
+"example": "hello there, Katie!",
+"synonyms": [],
+"antonyms": []
+}
+]
+},
+{
+"partOfSpeech": "noun",
+"definitions": [
+{
+"definition": "an utterance of ‘hello’; a greeting.",
+"example": "she was getting polite nods and hellos from people",
+"synonyms": [],
+"antonyms": []
+}
+]
+},
+{
+"partOfSpeech": "verb",
+"definitions": [
+{
+"definition": "say or shout ‘hello’.",
+"example": "I pressed the phone button and helloed",
+"synonyms": [],
+"antonyms": []
+}
+]
+}
+]
+}
+]
+
+Note: Word objects can have multiple objects in the meanings array. The application will show every meaning in the array.
+
+If the word does not exist, the error message below will be shown:
+
+Sorry pal, we couldn't find definitions for the word you were looking for.
+
+# Features
+
+Home Page:
+
+1. Locate the search bar and submit button at the top of the screen
+2. Type a word into the search bar, then press Submit. Pressing the "Enter" key on the keyboard will not submit the word.
+3. The word and all definitions will appear under the search bar.
+   -If the word does not exist, the error message will appear. This error message is sent from the API.
+
+Favorites Page:
+
+Shows all favorited words with their details. User currently cannot add or delete favorited words.
+
+Word of the Day Page:
+
+Shows a word and all of its definitions. The server sends a new word every 24 hours.
+
+About Page
+
+Shows the following information about this project:
+
+- Project Overview
+- Languages Used
+- Tools Used
+- What I Learned
+- Future Improvements
+
+# Design and Accessibility Considerations
+
+- Semantic HTML used for better screen reader support
+- Clear typography and spacing for readability
 
 # What I Learned
 
-I expanded my knowledge of vanilla Javascript. Now, I am more confident in my ability to build websites using this alone. I also apply my experience whenever I use Javascript based frameworks, such as React and Angular.
+- Work with REST APIs and handle asynchronous data
+- Structure frontend applications without frameworks
+- Translate API responses into user-friendly UI components
+- Debug real-world issues such as failed requests and missing data
 
 # Future Improvements
 
-In the future, I would like to implement a framework (React, Angular) that would allow me to make the page more dynamic.
+- Implement a framework (React, Angular) that would allow me to make the page more dynamic.
+
+- Implement the audio files that the API provides.
+
+- Implement the Favorites feature, which would allow users to save words to their account for future reference. Would also implement delete features to remove favorited words.
--- index.js
+++ index.js
@@ -7,21 +7,22 @@ let defListDiv = document.getElementById("definitionListDiv");
 let noWordEl;
 let wordDefEL;
 
-function getWordDefinition() {
-  /* clear word divs  */
-  if (!wordDefMainEl.hidden) {
-    wordDefMainEl.hidden = true;
-    if (noWordEl) {
-      console.log("entered", wordDefMainEl.lastChild);
-      wordDefMainEl.removeChild(wordDefMainEl.lastChild);
-    }
-  }
-  if (!fetchedWordEl.hidden) {
-    fetchedWordEl.hidden = true;
-    defListDiv.removeChild(wordDefEL);
+function clearPreviousResult() {
+  if (noWordEl) {
+    noWordEl.remove();
+    noWordEl = null;
   }
 
-  /* Fetch api */
+  defListDiv.innerHTML = "";
+  wordSpanEl.textContent = "";
+  wordPhonEl.textContent = "";
+  fetchedWordEl.hidden = true;
+  wordDefMainEl.hidden = true;
+}
+
+function getWordDefinition() {
+  clearPreviousResult();
+
   let wordInput = document.getElementById("wordInput");
   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput.value}`)
     .then((response) =>
@@ -29,86 +30,77 @@ function getWordDefinition() {
     )
     .then(({ ok, data }) => {
       if (!ok) {
-        wordSpanEl.textContent = null;
-        wordPhonEl.textContent = null;
         noWordEl = document.createElement("h4");
         noWordEl.textContent = data.message;
         wordDefMainEl.appendChild(noWordEl);
-      } else {
-        data.forEach((word) => {
-          console.log("word", word.phonetics);
-          wordDefEL = document.createElement("div");
-          wordDefEL.classList.add("definiton-div");
-          defListDiv.appendChild(wordDefEL);
-          wordSpanEl.textContent = word.word;
-          word.phonetics.forEach((phon) => {
-            if (phon.text) {
-              wordPhonEl.textContent = phon.text;
-              return;
-            }
-          });
-
-          word.meanings.forEach((meaning) => {
-            console.log("meaning", meaning);
-            let seperatorDiv = document.createElement("div");
-            seperatorDiv.classList.add("def-seperator");
-            let partOfSpeechEl = document.createElement("span");
-            partOfSpeechEl.classList.add("part-of-speech", "fs-5");
-            console.log(seperatorDiv, partOfSpeechEl);
-
-            let lineEl = document.createElement("hr");
-            lineEl.classList.add("w-50");
-            partOfSpeechEl.textContent = meaning.partOfSpeech;
-            seperatorDiv.appendChild(partOfSpeechEl);
-            seperatorDiv.appendChild(lineEl);
-            wordDefEL.appendChild(seperatorDiv);
-
-            /* create and append meaning span */
-            let meaningEl = document.createElement("span");
-            meaningEl.classList.add("fs-6", "text-secondary");
-            meaningEl.textContent = "Meaning";
-            wordDefEL.appendChild(meaningEl);
-
-            /* create and append definitons */
-
-            let defUlList = document.createElement("ul");
-            meaning.definitions.forEach((def) => {
-              let defListItem = document.createElement("li");
-              defListItem.textContent = def.definition;
-              defUlList.appendChild(defListItem);
-            });
-            wordDefEL.appendChild(defUlList);
-
-            /* if synonyms exist, create and append synonyms list*/
-
-            if (meaning.synonyms.length > 0) {
-              let synonymsDiv = document.createElement("div");
-              synonymsDiv.classList.add("d-flex", "gap-3");
-
-              let synonymSpanEl = document.createElement("span");
-              synonymSpanEl.classList.add("fs-6", "text-secondary");
-              synonymSpanEl.textContent = "Synonyms:";
-              synonymsDiv.appendChild(synonymSpanEl);
-
-              let synonymsListDiv = document.createElement("div");
-              synonymsListDiv.classList.add("w-75");
-              meaning.synonyms.forEach((synonym) => {
-                let synonymItemEl = document.createElement("span");
-                synonymItemEl.textContent = `${synonym} ${meaning.synonyms.length > 1 ? "," : ""} `;
-                synonymsListDiv.appendChild(synonymItemEl);
-              });
-
-              synonymsDiv.appendChild(synonymsListDiv);
-              wordDefEL.appendChild(synonymsDiv);
-            }
-          });
-        });
-
-        fetchedWordEl.hidden = false;
+        wordDefMainEl.hidden = false;
+        return;
       }
 
+      data.forEach((word) => {
+        wordDefEL = document.createElement("div");
+        wordDefEL.classList.add("definiton-div");
+        defListDiv.appendChild(wordDefEL);
+        wordSpanEl.textContent = word.word;
+
+        word.phonetics.forEach((phon) => {
+          if (phon.text) {
+            wordPhonEl.textContent = phon.text;
+            return;
+          }
+        });
+
+        word.meanings.forEach((meaning) => {
+          let seperatorDiv = document.createElement("div");
+          seperatorDiv.classList.add("def-seperator");
+          let partOfSpeechEl = document.createElement("span");
+          partOfSpeechEl.classList.add("part-of-speech", "fs-5");
+
+          let lineEl = document.createElement("hr");
+          lineEl.classList.add("w-50");
+          partOfSpeechEl.textContent = meaning.partOfSpeech;
+          seperatorDiv.appendChild(partOfSpeechEl);
+          seperatorDiv.appendChild(lineEl);
+          wordDefEL.appendChild(seperatorDiv);
+
+          let meaningEl = document.createElement("span");
+          meaningEl.classList.add("fs-6", "text-secondary");
+          meaningEl.textContent = "Meaning";
+          wordDefEL.appendChild(meaningEl);
+
+          let defUlList = document.createElement("ul");
+          meaning.definitions.forEach((def) => {
+            let defListItem = document.createElement("li");
+            defListItem.textContent = def.definition;
+            defUlList.appendChild(defListItem);
+          });
+          wordDefEL.appendChild(defUlList);
+
+          if (meaning.synonyms.length > 0) {
+            let synonymsDiv = document.createElement("div");
+            synonymsDiv.classList.add("d-flex", "gap-3");
+
+            let synonymSpanEl = document.createElement("span");
+            synonymSpanEl.classList.add("fs-6", "text-secondary");
+            synonymSpanEl.textContent = "Synonyms:";
+            synonymsDiv.appendChild(synonymSpanEl);
+
+            let synonymsListDiv = document.createElement("div");
+            synonymsListDiv.classList.add("w-75");
+            meaning.synonyms.forEach((synonym) => {
+              let synonymItemEl = document.createElement("span");
+              synonymItemEl.textContent = `${synonym} ${meaning.synonyms.length > 1 ? "," : ""} `;
+              synonymsListDiv.appendChild(synonymItemEl);
+            });
+
+            synonymsDiv.appendChild(synonymsListDiv);
+            wordDefEL.appendChild(synonymsDiv);
+          }
+        });
+      });
+
+      fetchedWordEl.hidden = false;
       wordDefMainEl.hidden = false;
-      console.log(wordDefMainEl);
     });
 }
 
